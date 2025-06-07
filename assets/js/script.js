c8c9c59f017203e87aa9b42a57393f43faa07c5d
pimdireto <pimdireto@gmail.com>
2025-06-07 00:32:32 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ function enviarFormulario() {
     }
   }, 100);
 
-  const URL_API = "https://script.google.com/macros/s/AKfycbyUCX-hXdXwRL4lJR_5W03LdDH86Vi3DBTmTkZ9rG8xALQZjVaFK-0dcSPzA-hh0B_u3g/exec";
+  const URL_API = "https://script.google.com/macros/s/AKfycbzsi2VbTTYZhg1cVZAiJGlR6yZJqppppAYWiOh1R3-jf8D0PzE5I2mcTb3f6NWXnpz-Wg/exec";
 
   // Primeiro, verifica se o RA já tem agendamento
   fetch(`${URL_API}?ra=${ra}`)
@@ -190,7 +190,7 @@ function consultarAgendamento() {
   // Mostra o carregamento opcional
   document.getElementById("loadingOverlay").style.display = "flex";
 
-  const URL_API = "https://script.google.com/macros/s/AKfycbyUCX-hXdXwRL4lJR_5W03LdDH86Vi3DBTmTkZ9rG8xALQZjVaFK-0dcSPzA-hh0B_u3g/exec";
+  const URL_API = "https://script.google.com/macros/s/AKfycbzsi2VbTTYZhg1cVZAiJGlR6yZJqppppAYWiOh1R3-jf8D0PzE5I2mcTb3f6NWXnpz-Wg/exec";
 
   fetch(`${URL_API}?ra=${ra}`)
     .then(res => res.json())
